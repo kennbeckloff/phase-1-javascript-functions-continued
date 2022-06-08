1ec1b28d1b50155b169b927349b0615c7a883da9
kennbeckloff <kennbeckloff688@gmail.com>
2022-06-08 11:45:33 +0300

    commit tree 8b2c2076638fe4f3247ac7646fb0dad52708cc6e

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun (event ='roller-skate'){
+    return `This Saturday, I want to ${event}!`; 
+    
+}
+ saturdayFun("bathe my dog!");
+
+// monday
+function mondayWork (event = 'go to the office'){
+    return `This Monday, I will ${event}.`;
+}
+mondayWork("Work from home");
+
+// wrapadjective
+const wrapAdjective = function (event = "*"){
+    return function (dd = 'a dedicated programmer'){
+        return `You are ${event}${dd}${event}!`;
+
+    }
+    
+}
